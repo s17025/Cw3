587b1325124f87c5cb3f9713e74ade5e0eb2fb27
s17025 <61729906+s17025@users.noreply.github.com>
2020-03-17 12:51:39 +0100

    1

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route(template: "api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IDbService dbService;
+
+        public StudentsController(IDbService db)
+        {
+            dbService = db;
+        }
+
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski, Leontiev sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Majewski");
+            }
+            return NotFound("Nie znaleziono studenta!");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut]
+        public IActionResult ModifyStudents(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Kowalski zmieniony na Majewski");
+            }else if(id == 2)
+            {
+                return Ok("Majewski zmieniony na Kowalski");
+            }
+
+            return NotFound("Nie znaleziono studenta!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski usuniety");
+            }
+            else if (id == 2)
+            {
+                return Ok("Majewski usuniety");
+            }
+
+            return NotFound("Nie znaleziono studenta!");
+        }
+
+
+    }
+}
---
+++ Cw3/Cw3/DAL/MockDbService.cs
@@ -0,0 +1,29 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> students;
+
+        static MockDbService()
+        {
+            students = new List<Student>
+            {
+                new Student(1, "Jan", "Kowalski"),
+                new Student(2, "Tomasz", "Smyrak"),
+                new Student(3, "Martyna", "Cwik"),
+
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return students;
+        }
+    }
+}
